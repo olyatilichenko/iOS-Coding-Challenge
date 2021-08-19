982201291d2045d5de356397134216209ac0cd7b
olgatilichenko <45556167+olgatilichenko@users.noreply.github.com>
2021-08-19 20:19:58 +0200

    Initial Commit

---
+++ iOS Coding Challenge/iOS_Coding_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Coding_ChallengeApp.swift
+//  iOS Coding Challenge
+//
+//  Created by Olga Tilichenko on 19.08.21.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Coding_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
